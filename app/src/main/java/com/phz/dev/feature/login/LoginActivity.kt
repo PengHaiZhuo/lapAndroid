feab1affdb1cc96f349de4f2c07d79d4449d6093
haizhuo001 <411593737@qq.com>
2021-08-17 18:04:24 +0800

    normal commit

---
+++ app/src/main/java/com/phz/dev/feature/login/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.phz.dev.feature.login
+
+import android.os.Bundle
+import com.phz.common.page.activity.BaseVmDbActivity
+import com.phz.dev.databinding.ActivityLoginBinding
+
+/**
+ * @author phz on 2021/8/17
+ * @description 登录页
+ */
+class LoginActivity : BaseVmDbActivity<LoginViewModel, ActivityLoginBinding>() {
+    override fun initData() {
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+    }
+}
---
+++ app/src/main/java/com/phz/dev/feature/login/LoginViewModel.kt
@@ -0,0 +1,10 @@
+package com.phz.dev.feature.login
+
+import com.phz.common.state.BaseViewModel
+
+/**
+ * @author phz on 2021/8/17
+ * @description
+ */
+class LoginViewModel:BaseViewModel() {
+}
