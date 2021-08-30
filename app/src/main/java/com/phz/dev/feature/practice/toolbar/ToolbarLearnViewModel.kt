f1d282640c20085dc81009986623154bcf4c1123
haizhuo001 <411593737@qq.com>
2021-08-30 15:32:38 +0800

    feat:toolbar practice

--- app/src/main/java/com/phz/dev/feature/practice/toolbar/ToolbarLearnViewModel.kt
+++ app/src/main/java/com/phz/dev/feature/practice/toolbar/ToolbarLearnViewModel.kt
@@ -1,10 +1,13 @@
 package com.phz.dev.feature.practice.toolbar
 
 import androidx.lifecycle.ViewModel
+import com.phz.common.livedata.BooleanLiveData
 
 /**
  * @author phz on 2021/8/27
  * @description
  */
 class ToolbarLearnViewModel:ViewModel() {
+    //默认未收藏
+    var isCollect=BooleanLiveData()
 }
