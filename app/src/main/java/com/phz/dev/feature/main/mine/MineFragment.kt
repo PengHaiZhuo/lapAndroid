38bab0c08761656ed982806e045284ca84c83ff6
haizhuo001 <411593737@qq.com>
2021-09-14 17:24:21 +0800

    normal commit

--- app/src/main/java/com/phz/dev/feature/main/mine/MineFragment.kt
+++ app/src/main/java/com/phz/dev/feature/main/mine/MineFragment.kt
@@ -1,6 +1,9 @@
 package com.phz.dev.feature.main.mine
 
+import android.graphics.Color
 import android.os.Bundle
+import androidx.core.view.size
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.gyf.immersionbar.ktx.immersionBar
 import com.gyf.immersionbar.ktx.setFitsSystemWindows
 import com.phz.common.ext.startKtxActivity
@@ -23,10 +26,6 @@ class MineFragment : BaseVmDbPureFragment<MineViewModel, FragmentMineBinding>() 
 //        mViewDataBinding.btnToChild.clickNoRepeat {
 //            nav().navigate(R.id.action_mine_to_child)
 //        }
-        mViewDataBinding.swipeRl.setOnRefreshListener {
-            //todo 从网络获取积分 返回结果后手动停止动画
-//            mViewDataBinding.swipeRl.isRefreshing=false
-        }
     }
 
     override fun onResume() {
@@ -40,6 +39,21 @@ class MineFragment : BaseVmDbPureFragment<MineViewModel, FragmentMineBinding>() 
     override fun initView(savedInstanceState: Bundle?) {
         mViewDataBinding.vm = mViewModel
         mViewDataBinding.clickProxy = ProxyClick()
+        mViewDataBinding.swipeRl.apply {
+//            setSize(SwipeRefreshLayout.LARGE) //设置尺寸
+//            setColorSchemeColors(Color.RED, Color.YELLOW, Color.BLUE);//设置刷新时颜色变换
+            setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light)//设置刷新时颜色变换
+            setBackgroundColor(Color.WHITE)//设置背景色
+            setDistanceToTriggerSync(96)//设置下拉距离
+            setOnRefreshListener {
+                //todo 从网络获取积分 返回结果后手动停止动画
+//            mViewDataBinding.swipeRl.isRefreshing=false
+            }
+        }
     }
 
 
