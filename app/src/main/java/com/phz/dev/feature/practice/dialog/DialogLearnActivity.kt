afae3a262d2850436c744715ece37be8ad7f6bf0
haizhuo001 <411593737@qq.com>
2021-09-16 15:21:22 +0800

    feat:dialog learn

--- app/src/main/java/com/phz/dev/feature/practice/dialog/DialogLearnActivity.kt
+++ app/src/main/java/com/phz/dev/feature/practice/dialog/DialogLearnActivity.kt
@@ -3,11 +3,15 @@ package com.phz.dev.feature.practice.dialog
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
+import com.phz.common.databinding.MineBindingAdapter.circleImageUrlRes
+import com.phz.common.databinding.MineBindingAdapter.imageGifUrlRes
+import com.phz.common.ext.logE
 import com.phz.common.ext.startKtxActivity
 import com.phz.common.page.activity.BaseVmDbActivity
 import com.phz.common.state.BaseViewModel
 import com.phz.dev.R
 import com.phz.dev.databinding.ActivityDialogLearnBinding
+import com.phz.dev.databinding.CustomGenshinUserReflectBinding
 import com.phz.dev.feature.practice.dialog.activity.DialogSampleActivity
 import com.phz.dev.feature.practice.dialog.fragment.DialogSampleFragment
 
@@ -17,13 +21,16 @@ import com.phz.dev.feature.practice.dialog.fragment.DialogSampleFragment
  * ①普通Dialog（标题+正文+按钮）传统单选列表、永久单选列表、永久多选列表
  * ②DialogFragment
  * ③Activity样式
+ * ④自定义customView
+ * 补充：推荐使用开源库👉https://github.com/afollestad/material-dialogs
  */
 class DialogLearnActivity : BaseVmDbActivity<BaseViewModel, ActivityDialogLearnBinding>() {
-    private val dialogFragment by lazy { DialogSampleFragment() }
     private var normalAlertDialog: AlertDialog? = null
     private var itemsAlertDialog: AlertDialog? = null
     private var singleChoiceItemsAlertDialog: AlertDialog? = null
     private var multiItemsAlertDialog: AlertDialog? = null
+    private val dialogFragment by lazy { DialogSampleFragment() }
+    private var customViewAlertDialog: AlertDialog? = null
 
     override fun initData() {
         //普通Dialog
@@ -54,9 +61,6 @@ class DialogLearnActivity : BaseVmDbActivity<BaseViewModel, ActivityDialogLearnB
                 )
                 //适配器只需要是ListAdapter即可
                 setAdapter(adapter) { dialog, _ ->
-                    dialog.dismiss()
-                }
-                setPositiveButton(R.string.confirm) { dialog, _ ->
                     dialog.dismiss()
                 }
                 setNegativeButton(R.string.cancel) { dialog, _ ->
@@ -91,10 +95,29 @@ class DialogLearnActivity : BaseVmDbActivity<BaseViewModel, ActivityDialogLearnB
                 }
             }
         multiItemsAlertDialog = multiItemsBuilder.create()
+        //Custom View
+        val customBuilder = AlertDialog.Builder(this@DialogLearnActivity)
+            .apply {
+                val binding = CustomGenshinUserReflectBinding.inflate(layoutInflater)
+                setView(binding.root)
+                imageGifUrlRes(binding.ivCustomGenshinRefBg, R.raw.xiangling)
+                circleImageUrlRes(binding.ivCustomGenshinRefAvatar, R.drawable.ic_genshin_k)
+                binding.btCustomGenshinUserRefConfirm.setOnClickListener {
+                    "邮箱：${binding.tvInputCustomGenshinMail.text} 反馈：${binding.tvInputCustomGenshinReflect.text}".logE()
+                    customViewAlertDialog?.dismiss()
+                }
+                binding.btCustomGenshinUserRefCancel.setOnClickListener {
+                    customViewAlertDialog?.dismiss()
+                }
+            }
+        customViewAlertDialog = customBuilder.create()
     }
 
     override fun initView(savedInstanceState: Bundle?) {
         mViewDataBinding.click = ClickProxy()
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_left_white)
+        mToolbar.setNavigationOnClickListener { onBackPressed() }
+        centerTextView.text = "Dialog"
     }
 
     inner class ClickProxy {
@@ -122,8 +145,8 @@ class DialogLearnActivity : BaseVmDbActivity<BaseViewModel, ActivityDialogLearnB
             startKtxActivity<DialogSampleActivity>()
         }
 
-        fun clickCustomView(){
-
+        fun clickCustomView() {
+            customViewAlertDialog?.show()
         }
     }
 }
