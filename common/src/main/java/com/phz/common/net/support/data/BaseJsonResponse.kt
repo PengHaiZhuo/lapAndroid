3f115c957ddf4369388e15fdab971e5270606073
haizhuo001 <411593737@qq.com>
2021-12-23 16:26:39 +0800

    normal commit

--- common/src/main/java/com/phz/common/net/support/data/BaseJsonResponse.kt
+++ common/src/main/java/com/phz/common/net/support/data/BaseJsonResponse.kt
@@ -4,7 +4,7 @@ package com.phz.common.net.support.data
  * @author phz
  * @description 服务器返回数据基类
  * 一般情况下 json数据格式返回都有code、data、message，具体命名和数据结构看服务端
- * @notice 继承此类，重写各个方法，设置返回字段名，设置是否成功（比如json字段中的code为200代表这次请求成功）
+ * @notice 继承此类，重写各个方法，设置返回字段名，设置成功条件（比如getResponseCode为200代表请求成功）
  */
 abstract class BaseJsonResponse<T> {
     abstract fun isSuccess():Boolean
