2d231b7786eeadb8d9e9d2e34033f50545b25d80
haizhuo001 <411593737@qq.com>
2022-09-24 16:03:04 +0800

    feat：新增隐私政策和用户协议弹框

--- app/src/main/java/com/phz/dev/feature/practice/dialog/DialogLearnActivity.kt
+++ app/src/main/java/com/phz/dev/feature/practice/dialog/DialogLearnActivity.kt
@@ -1,19 +1,27 @@
 package com.phz.dev.feature.practice.dialog
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
 import com.phz.common.databinding.MineBindingAdapter.circleImageUrlRes
 import com.phz.common.databinding.MineBindingAdapter.imageGifUrlRes
 import com.phz.common.ext.logE
+import com.phz.common.ext.showSnackShort
 import com.phz.common.ext.startKtxActivity
 import com.phz.common.page.activity.BaseToolbarActivity
 import com.phz.common.state.NoViewModel
 import com.phz.dev.R
 import com.phz.dev.databinding.ActivityDialogLearnBinding
+import com.phz.dev.databinding.CustomFirstUseBinding
 import com.phz.dev.databinding.CustomGenshinUserReflectBinding
 import com.phz.dev.feature.practice.dialog.activity.DialogSampleActivity
 import com.phz.dev.feature.practice.dialog.fragment.DialogSampleFragment
+import com.phz.dev.feature.wp.WebViewActivity
 
 /**
  * @author phz on 2021/9/14
@@ -31,6 +39,7 @@ class DialogLearnActivity : BaseToolbarActivity<NoViewModel, ActivityDialogLearn
     private var multiItemsAlertDialog: AlertDialog? = null
     private val dialogFragment by lazy { DialogSampleFragment() }
     private var customViewAlertDialog: AlertDialog? = null
+    private var customViewAlertDialogP: AlertDialog? = null
 
     override fun initData() {
         //普通Dialog
@@ -111,6 +120,57 @@ class DialogLearnActivity : BaseToolbarActivity<NoViewModel, ActivityDialogLearn
                 }
             }
         customViewAlertDialog = customBuilder.create()
+        //Custom View P
+        val customBuilderP = AlertDialog.Builder(this@DialogLearnActivity)
+            .apply {
+                val binding = CustomFirstUseBinding.inflate(layoutInflater)
+                setView(binding.root)
+                setCancelable(false)
+                //设置超链接点击
+                binding.tvContent.apply {
+                    movementMethod=LinkMovementMethod.getInstance()
+                    setLinkTextColor(getColor(R.color.colorBlue66))
+                }
+                val ss=getString(R.string.tip_first_use)
+                val privacy ="《隐私政策》"
+                val agreement ="《用户协议》"
+                val startIndexPrivacy=ss.indexOf(privacy)
+                val endIndexPrivacy=startIndexPrivacy+privacy.length
+                val startIndexAgreement =ss.indexOf(agreement)
+                val endIndexAgreement =startIndexAgreement+agreement.length
+                var spanString= SpannableString(ss)
+                //设置span
+                spanString.apply {
+                    setSpan(object :ClickableSpan(){
+                        override fun onClick(widget: View) {
+                            var lists = arrayListOf(
+                                Pair(WebViewActivity.FLAG_TITLE, getString(R.string.privacy)),
+                                Pair(WebViewActivity.FLAG_ASSET, "file:///android_asset/privacy.html")
+                            )
+                            startKtxActivity<WebViewActivity>(values = lists)
+                        }
+                    },startIndexPrivacy,endIndexPrivacy,Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+                    setSpan(object :ClickableSpan(){
+                        override fun onClick(widget: View) {
+                            var lists = arrayListOf(
+                                Pair(WebViewActivity.FLAG_TITLE, getString(R.string.agreement)),
+                                Pair(WebViewActivity.FLAG_ASSET, "file:///android_asset/agreement.html")
+                            )
+                            context!!.startKtxActivity<WebViewActivity>(values = lists)
+                        }
+                    },startIndexAgreement,endIndexAgreement,Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+                }
+                binding.tvContent.text=spanString
+                binding.btAgree.setOnClickListener {
+                    showSnackShort(getString(R.string.agree))
+                    customViewAlertDialogP?.dismiss()
+                }
+                binding.btNoAgree.setOnClickListener {
+                    showSnackShort(getString(R.string.no_agree))
+                    customViewAlertDialogP?.dismiss()
+                }
+            }
+        customViewAlertDialogP = customBuilderP.create()
     }
 
     override fun initView(savedInstanceState: Bundle?) {
@@ -148,5 +208,10 @@ class DialogLearnActivity : BaseToolbarActivity<NoViewModel, ActivityDialogLearn
         fun clickCustomView() {
             customViewAlertDialog?.show()
         }
+
+        fun clickCustomViewP() {
+            customViewAlertDialogP?.show()
+        }
+
     }
 }
